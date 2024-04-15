42c6e05b4e8d5d1233b814c933f5e92e621aa108
0xsatoshi <zeroxsatoshi@gmail.com>
2024-04-15 23:17:02 +0800

    update displaydenom

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -30,7 +30,7 @@ const (
 
 const (
 	// DisplayDenom defines the denomination displayed to users in client applications.
-	DisplayDenom = "evmos"
+	DisplayDenom = "a0gi"
 	// BaseDenom defines to the default denomination used in Evmos (staking, EVM, governance, etc.)
 	BaseDenom = "neuron"
 )
